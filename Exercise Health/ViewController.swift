2457b8d7bb946778edfbec5558f4bd6e27d8b4cb
hlerner <hannahlerner1@gmail.com>
2018-03-13 00:38:23 -0400

    Initial Commit

---
+++ Exercise Health/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercise Health
+//
+//  Created by Hannah Lerner on 3/13/18.
+//  Copyright © 2018 Hannah Lerner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
